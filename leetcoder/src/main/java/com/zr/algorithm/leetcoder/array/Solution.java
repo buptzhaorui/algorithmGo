116d3970e81a0cb649c46b9eecabdf33ec370ec7
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-09-01 19:28:09 +0800

    array remove element

--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Solution.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Solution.java
@@ -60,14 +60,40 @@ public class Solution {
     return i;
   }
 
+  public int countPrimes(int n) {
+    if(n >= 2){
+      int size = 1;
+      int[] primes = new int[n];
+      primes[size-1] = 2;
+
+      int i = 3;
+      while(i <= n){
+        int j = 0;
+        for( ; j < size; j++){
+          if(i % (primes[j]) == 0) break;
+        }
+        if(j == size){
+          primes[size] = i;
+          size++;
+        }
+        i += 2;
+      }
+      return size;
+    }else{
+      return 0;
+    }
+  }
+
 
   public static void main(String[] args) {
     int[] nums = new int[]{3,2,34,2,2};
     Solution solution = new Solution();
-    int count = solution.removeElement3(nums, 2);
-    System.out.println("count: " + count);
-    for(int i = 0; i < count; i++){
-      System.out.println(nums[i]);
-    }
+//    int count = solution.removeElement3(nums, 2);
+//    System.out.println("count: " + count);
+//    for(int i = 0; i < count; i++){
+//      System.out.println(nums[i]);
+//    }
+    System.out.print(solution.countPrimes(6));
+
   }
 }
