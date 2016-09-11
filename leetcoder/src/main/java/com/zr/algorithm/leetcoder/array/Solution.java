facd64137b77195988ac03e869b24f995df7a2c5
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-09-11 17:37:31 +0800

    ''

--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Solution.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Solution.java
@@ -520,6 +520,51 @@ public class Solution {
     else return -1;
   }
 
+
+  public int uniquePaths(int m, int n) {
+    int[][] arr = new int[m][n];
+    for(int i = 0; i < m; i++) arr[i][0] = 1;
+    for(int j = 0; j < n; j++) arr[0][j] = 1;
+    for(int i = 1; i < m; i++){
+      for(int j = 1; j < n; j++){
+        arr[i][j] = arr[i][j-1] + arr[i-1][j];
+      }
+    }
+
+    return arr[m-1][n-1];
+  }
+
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int m = obstacleGrid.length;
+    int n = obstacleGrid[0].length;
+    if(obstacleGrid[0][0] == 1)
+      obstacleGrid[0][0] = 0;
+    else
+      obstacleGrid[0][0] = 1;
+    for(int i = 1; i < m; i++){
+      if (obstacleGrid[i][0] == 1)
+        obstacleGrid[i][0] = 0;
+      else
+        obstacleGrid[i][0] = obstacleGrid[i-1][0];
+    }
+    for(int j = 1; j < n; j++){
+      if (obstacleGrid[0][j] == 1)
+        obstacleGrid[0][j] = 0;
+      else
+        obstacleGrid[0][j] = obstacleGrid[0][j-1];
+    }
+    for(int i = 1; i < m; i++){
+      for(int j = 1; j < n; j++){
+        if(obstacleGrid[i][j] == 1)
+          obstacleGrid[i][j] = 0;
+        else
+          obstacleGrid[i][j] = obstacleGrid[i][j-1] + obstacleGrid[i-1][j];
+      }
+    }
+
+    return obstacleGrid[m-1][n-1];
+  }
+
   public static void main(String[] args) {
     int[] nums = new int[]{3,2,34,2,2};
     Solution solution = new Solution();
