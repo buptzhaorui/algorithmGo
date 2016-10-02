97a392ebb067cb8be603fa9b063e98bd40f1e697
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-10-02 16:22:17 +0800

    ''

--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Solution.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Solution.java
@@ -1087,6 +1087,25 @@ public class Solution {
     return rst;
   }
 
+  public int[] productExceptSelf(int[] nums) {
+    int n = nums.length;
+    int[] rst = new int[n];
+    if(n == 0) return rst;
+
+    rst[0] = 1;
+    for(int i = 1; i < n; i++){
+      rst[i] = rst[i-1] * nums[i-1];
+    }
+
+    int right = 1;
+    for(int i = n-1; i >= 0; i--){
+      rst[i] *= right;
+      right *= nums[i];
+    }
+
+    return rst;
+  }
+
   public static void main( String[] args) {
     int[] nums = new int[]{3,2,34,2,2};
     Solution solution = new Solution();
