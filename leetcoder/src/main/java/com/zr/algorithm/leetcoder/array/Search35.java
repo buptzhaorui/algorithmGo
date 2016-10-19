c7724a0b3d250265a80a7373487934f0264e411b
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-10-19 21:57:47 +0800

    ''

---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Search35.java
@@ -0,0 +1,12 @@
+package com.zr.algorithm.leetcoder.array;
+
+/**
+ * User: zhaorui
+ * Date: 2016/10/19
+ * Time: 21:46
+ */
+public class Search35 {
+  public int searchInsert(int[] nums, int target) {
+
+  }
+}
