0299fced93ff2e98e96c3735fbeed656c0d64d65
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-10-30 10:23:44 +0800

    ''

---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/array/Longest409.java
@@ -0,0 +1,45 @@
+package com.zr.algorithm.leetcoder.array;
+
+/**
+ * User: zhaorui
+ * Date: 2016/10/30
+ * Time: 9:22
+ */
+public class Longest409 {
+  public int longestPalindrome(String s) {
+    int rst = 0;
+    int n = s.length();
+    int[] tmp = new int[256];
+    for(int i = 0; i < n; i++){
+      int value = tmp[s.charAt(i)] ^ 1;
+      if(value == 0){
+        rst ++;
+      }
+      tmp[s.charAt(i)] = value;
+    }
+
+    rst *= 2;
+    if(rst < n) rst ++;
+
+    return rst;
+  }
+
+  public int longestPalindrome2(String s) {
+    if (s == null || s.length() == 0) return 0;
+    if (s.length() == 1) return 1;
+
+    int result = 0;
+    int[] counter = new int[128];
+
+    for (char c : s.toCharArray()) {
+      if(counter[c] == 0) {
+        counter[c] = 1;
+      } else {
+        counter[c] = 0;
+        result += 2;
+      }
+    }
+
+    return Math.min(result + 1, s.length());
+  }
+}
