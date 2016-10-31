e0605b354c76082f0282d61322d21cf083d845ec
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-10-31 19:52:52 +0800

    ''

--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/hashtable/Longest3.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/hashtable/Longest3.java
@@ -31,4 +31,15 @@ public class Longest3 {
     }
     return rst;
   }
+
+  public int lengthOfLongestSubstring2(String s) {
+    int result = 0;
+    int[] cache = new int[256];
+    for (int i = 0, j = 0; i < s.length(); i++) {
+      j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
+      cache[s.charAt(i)] = i + 1;
+      result = Math.max(result, i - j + 1);
+    }
+    return result;
+  }
 }
---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/hashtable/Repeated187.java
@@ -0,0 +1,14 @@
+package com.zr.algorithm.leetcoder.hashtable;
+
+import java.util.List;
+
+/**
+ * User: zhaorui
+ * Date: 2016/10/31
+ * Time: 19:17
+ */
+public class Repeated187 {
+  public List<String> findRepeatedDnaSequences(String s) {
+
+  }
+}
