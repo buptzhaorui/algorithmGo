2ad11671a44cc682445acfd4e60c9f871f5d5bf2
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-11-02 22:56:28 +0800

    ''

--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/hashtable/Fraction166.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/hashtable/Fraction166.java
@@ -1,29 +1,47 @@
 package com.zr.algorithm.leetcoder.hashtable;
 
+import java.util.HashMap;
+
 /**
  * User: zhaorui
  * Date: 2016/11/1
  * Time: 16:53
  */
 public class Fraction166 {
-  public String fractionToDecimal(int numerator, int denominator) {
-    int integetPart = numerator / denominator;
-    int decimalPart = numerator % denominator;
+  private String positiveFractionToDecimal(long numerator, long denominator){
+    long integetPart = numerator / denominator;
+    long decimalPart = numerator % denominator;
     if(decimalPart == 0){
       return integetPart + "";
     }else {
       String rst = ".";
-      int[] v = new int[denominator];
+      HashMap<Long, Integer> map = new HashMap<>();
       int index = 1; //base on 1
-      int a, b = decimalPart;
-      while (b != 0 && v[b] == 0) {
-        v[b] = index;
+      long a, b = decimalPart;
+      while (b != 0 && !map.containsKey(b)) {
+        map.put(b, index++);
         a = b * 10 / denominator;
         b = b * 10 % denominator;
         rst += a;
       }
-      if (v[b] != 0) rst = rst.substring(0, v[b]) + "(" + rst.substring(v[b]) + ")";
+      if (map.containsKey(b)) {
+        int i = map.get(b);
+        rst = rst.substring(0, i) + "(" + rst.substring(i) + ")";
+      }
       return integetPart + rst;
     }
+  }
+  public String fractionToDecimal(long numerator, long denominator) {
+    if(numerator == 0) return "0";
+
+    if((numerator > 0 && denominator > 0) || numerator < 0 && denominator < 0){
+      return positiveFractionToDecimal(Math.abs(numerator), Math.abs(denominator));
+    }else{
+      return "-" + positiveFractionToDecimal(Math.abs(numerator), Math.abs(denominator));
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new Fraction166().fractionToDecimal(-1, -2147483648));
   }
 }
