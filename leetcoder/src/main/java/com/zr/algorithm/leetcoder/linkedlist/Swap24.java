ce6ed1bc07e09a6f412694022c37a5bdb26af36d
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-11-05 20:33:57 +0800

    ''

--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/linkedlist/Swap24.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/linkedlist/Swap24.java
@@ -6,4 +6,22 @@ package com.zr.algorithm.leetcoder.linkedlist;
  * Time: 19:47
  */
 public class Swap24 {
+  public ListNode swapPairs(ListNode head) {
+    ListNode p0 = null, p1, p2, p3;
+    p1 = head;
+    while(p1 != null && p1.next != null){
+      p2 = p1.next;
+      p3 = p2.next;
+      p2.next = p1;
+      p1.next = p3;
+      if(p1 == head){
+        head = p2;
+      }else{
+        p0.next = p2;
+      }
+      p0 = p1;
+      p1 = p3;
+    }
+    return head;
+  }
 }
