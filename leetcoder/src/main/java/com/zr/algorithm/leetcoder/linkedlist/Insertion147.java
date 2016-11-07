8d64a2e52c7d0fb29b52569e4fa552dbb95cb0eb
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-11-08 00:03:18 +0800

    ''

---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/linkedlist/Insertion147.java
@@ -0,0 +1,22 @@
+package com.zr.algorithm.leetcoder.linkedlist;
+
+/**
+ * User: zhaorui
+ * Date: 2016/11/7
+ * Time: 21:00
+ */
+public class Insertion147 {
+  public ListNode insertionSortList(ListNode head) {
+    if(head == null) return null;
+    ListNode cur = head.next;
+    head.next = null;
+    while(cur != null){
+      ListNode p = head;
+      while(p != null){
+
+      }
+    }
+
+    return head;
+  }
+}
