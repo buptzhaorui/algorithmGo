47bbc054837938ad8835d7ef245cb4118938f36c
buptzhaorui <buptzhaorui@foxmail.com>
2016-11-10 11:46:17 +0800

    ''

--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/linkedlist/Linked142.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/linkedlist/Linked142.java
@@ -6,4 +6,23 @@ package com.zr.algorithm.leetcoder.linkedlist;
  * Time: 23:17
  */
 public class Linked142 {
+  public ListNode detectCycle(ListNode head) {
+    ListNode slow, fast;
+    slow = fast = head;
+    while(fast != null && fast.next != null){
+      if(slow.next == fast.next.next){
+        slow = head;
+        fast = fast.next.next;
+        while(slow != fast){
+          slow = slow.next;
+          fast = fast.next;
+        }
+        return slow;
+      }else{
+        slow = slow.next;
+        fast = fast.next.next;
+      }
+    }
+    return null;
+  }
 }
