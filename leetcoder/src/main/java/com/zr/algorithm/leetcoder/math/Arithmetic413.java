4a076995959d2706e711577207350d4c348eb0bb
buptzhaorui <buptzhaorui@foxmail.com>
2016-12-13 17:30:00 +0800

    ''

---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/math/Arithmetic413.java
@@ -0,0 +1,14 @@
+package com.zr.algorithm.leetcoder.math;
+
+/**
+ * User: zhaorui
+ * Date: 2016/12/13
+ * Time: 17:13
+ */
+public class Arithmetic413 {
+  public int numberOfArithmeticSlices(int[] A) {
+    int num = 0;
+
+    return num;
+  }
+}
