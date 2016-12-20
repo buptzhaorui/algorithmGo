567fb1b3c1271c453eaca2c5a29c655896aa0129
buptzhaorui <buptzhaorui@foxmail.com>
2016-12-20 17:52:51 +0800

    ''

---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/pointers/Reverse345.java
@@ -0,0 +1,15 @@
+package com.zr.algorithm.leetcoder.pointers;
+
+/**
+ * User: zhaorui
+ * Date: 2016/12/20
+ * Time: 14:25
+ */
+public class Reverse345 {
+  private boolean isVowel(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+  }
+  public String reverseVowels(String s) {
+    StringBuilder sb = new StringBuilder();
+  }
+}
--- leetcoder/src/main/java/com/zr/algorithm/leetcoder/pointers/Valid125.java
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/pointers/Valid125.java
@@ -7,18 +7,43 @@ package com.zr.algorithm.leetcoder.pointers;
  */
 public class Valid125 {
   public boolean isPalindrome(String s) {
-    s = s.replaceAll("^w", "");
+    s = s.replaceAll("[^a-zA-Z0-9]", "");
     int left = 0;
     int right = s.length() - 1;
     while(left < right){
       char c1 = s.charAt(left++);
       char c2 = s.charAt(right--);
-      if(c1 >= '0' && c1 <= '9'){
+      if(c1 >= '0' && c1 <= '9' || c2 >= '0' && c2 <= '9'){
         if(c1 != c2) return false;
       }else{
-        if(!(c1 == c2 || c1 - c2 == 0 || c1 - c2 == 32 || c1 - c2 == -32)) return false;
+        if(!(c1 - c2 == 0 || c1 - c2 == 32 || c1 - c2 == -32)) return false;
       }
     }
+    return true;
+  }
+
+  public boolean isPalindrome2(String s) {
+    if (s.isEmpty()) {
+      return true;
+    }
+    int head = 0, tail = s.length() - 1;
+    char cHead, cTail;
+    while(head <= tail) {
+      cHead = s.charAt(head);
+      cTail = s.charAt(tail);
+      if (!Character.isLetterOrDigit(cHead)) {
+        head++;
+      } else if(!Character.isLetterOrDigit(cTail)) {
+        tail--;
+      } else {
+        if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+          return false;
+        }
+        head++;
+        tail--;
+      }
+    }
+
     return true;
   }
 }
