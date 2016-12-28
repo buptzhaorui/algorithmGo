d80b920f2014f3112ceda6c9ef7c7170e56c34f4
buptzhaorui <bupt_zhaorui@foxmail.com>
2016-12-28 19:42:46 +0800

    ''

---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/pointers/Reverse344.java
@@ -0,0 +1,12 @@
+package com.zr.algorithm.leetcoder.pointers;
+
+/**
+ * User: zhaorui
+ * Date: 2016/12/20
+ * Time: 20:56
+ */
+public class Reverse344 {
+  public String reverseString(String s) {
+    return new StringBuilder(s).reverse().toString();
+  }
+}
