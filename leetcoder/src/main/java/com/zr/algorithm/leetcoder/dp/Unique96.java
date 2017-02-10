d169b9e74a635024cbc4c9c83f6a4e37781811a6
buptzhaorui <bupt_zhaorui@foxmail.com>
2017-02-11 01:18:53 +0800

    ''

---
+++ leetcoder/src/main/java/com/zr/algorithm/leetcoder/dp/Unique96.java
@@ -0,0 +1,21 @@
+package com.zr.algorithm.leetcoder.dp;
+
+/**
+ * User: zhaorui
+ * Date: 2017/2/10
+ * Time: 17:45
+ */
+public class Unique96 {
+  public int numTrees(int n) {
+    int [] G = new int[n+1];
+    G[0] = G[1] = 1;
+
+    for(int i=2; i<=n; ++i) {
+      for(int j=1; j<=i; ++j) {
+        G[i] += G[j-1] * G[i-j];
+      }
+    }
+
+    return G[n];
+  }
+}
